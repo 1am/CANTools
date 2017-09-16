782b4a2e526b0f25923e63f4ad955f85f4f577e6
Piotr Barszczewski <piotr@1am.pl>
2017-09-16 22:08:51 +0200

    Using ICSim fork from Grazfather + a compilation error fix for non GUI ICSim controls build.

--- scripts/02-setup-user-tools.sh
+++ scripts/02-setup-user-tools.sh
@@ -15,6 +15,7 @@ pip install \
 mkdir -p ${HOME_DIR}/tools && cd ${HOME_DIR}/tools
 cd ${HOME_DIR}/tools
 
+# Cloning caringcaribou 
 if [ -d "caringcaribou" ]; then
     cd caringcaribou
     git pull
@@ -23,13 +24,26 @@ else
     git clone https://github.com/CaringCaribou/caringcaribou.git
 fi
 
+# Cloning modified ICSim version which doeesn't include GUI 
+# Not starting GUI for both icsim and control at once
+# makes the VM not freeze because of running 2 OpenGL windows.
 if [ -d "ICSim" ]; then
     cd ICSim
-    git pull
+    git pull origin master
     cd ..
 else
-    git clone https://github.com/zombieCraig/ICSim.git
+    # git clone https://github.com/zombieCraig/ICSim.git
+    git clone https://github.com/Grazfather/ICSim.git
 fi
+
+# Rebuild ICSim without GUI (see: https://github.com/zombieCraig/ICSim/pull/6)
+cd ICSim
+git checkout c291d623e516e5a8b69929ee94d5ad578a99e4d5 .
+# Make a no GUI version compile (see: https://github.com/zombieCraig/ICSim/pull/6#issuecomment-329991016)
+sed -i '967s/\t\}/\t\/\/\}/' controls.c
+make controls CFLAGS+=-Wno-error=misleading-indentation CFLAGS+=-DDISABLE_SDL=1
+make icsim
+cd ..
 
 # Create symlinks to commands
 # mkdir -p ${HOME_DIR}/bin
