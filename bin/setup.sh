a3edd5a6c83b9a9e8e7315c0683c2e050cc46b0f
Piotr Barszczewski <piotr@1am.pl>
2017-11-05 12:50:59 +0100

    Updating box name

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}/../box-cutter/ubuntu
 bin/box build ubuntu1704-desktop virtualbox
-vagrant box add box/virtualbox/ubuntu1704-desktop-17.0907.1.box --name CanToolsLinux
+vagrant box add box/virtualbox/ubuntu1704-desktop-17.0907.1.box --name CANToolsLinux
 
 cd ${DIR}/..
 vagrant plugin install vagrant-reload
